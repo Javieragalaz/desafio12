0a45a39a289a9888c2c5b56df045e8b6c35e6a15
Javiera Galaz <jgalazbecerra@gmail.com>
2023-02-05 16:58:13 -0300

    Parte 1

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+let photo=document.querySelector("img");
+
+photo.addEventListener("click", function borderColor()
+
+{
+
+    if (photo.style.border =="none") {
+        photo.style.border = "solid 2px red"
+
+    }
+    
+    else {
+        photo.style.border = "none"
+
+    }
+
+})
+
+
+
+
+
+
+
