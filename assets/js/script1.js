8a963dc3df0bd137be4220705e0e80bd718dd254
Javiera Galaz <jgalazbecerra@gmail.com>
2023-02-05 21:53:01 -0300

    Fin proyecto

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -5,23 +5,23 @@ const result = document.querySelector("#result")
 
 const verify = document.querySelector("button")
 
-verify.addEventListener("click", function verifyStickers(){
-    const a = Number (input1.value)
-    const b = Number (input2.value)
-    const c = Number (input3.value)
+verify.addEventListener("click", function verifyStickers() {
+    const a = Number(input1.value)
+    const b = Number(input2.value)
+    const c = Number(input3.value)
     const total = (a + b + c)
 
-    if  (total > 10) {
+    if (total > 10) {
         result.innerHTML = "Agregaste demasiados stickers D:"
-       
+
     } else if (total == 1) {
         result.innerHTML = `Has agegado ${total} sticker`
 
     } else if (total > 0) {
         result.innerHTML = `Has agegado ${total} stickers`
-     
+
     } else {
         result.innerHTML = "No has agregado stickers :("
-       
+
     }
 })
